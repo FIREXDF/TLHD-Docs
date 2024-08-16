01d7a10a23267964cd546cef9f84a687d217d9fe
FIREXDF <81977081+FIREXDF@users.noreply.github.com>
2024-08-16 21:24:31 +0000

    modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [starlight({
-    title: 'Tomodachi Life HD Docs',
+    title: ' ',
     logo: {
       src: './src/assets/logo.png'
     },
